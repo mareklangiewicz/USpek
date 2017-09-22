88b8ef2def83652f2dbb608c3b8c57c779892299
Paweł Gajda <pawegio@gmail.com>
2017-09-22 10:29:14 +0200

    Add few runner improvements

--- uspek/src/main/java/mareklangiewicz/pl/uspek/USpekJUnitRunner.kt
+++ uspek/src/main/java/mareklangiewicz/pl/uspek/USpekJUnitRunner.kt
@@ -1,23 +1,30 @@
 package mareklangiewicz.pl.uspek
 
 import org.junit.runner.Description
-import org.junit.runner.notification.Failure
 import org.junit.runner.notification.RunNotifier
 import org.junit.runners.BlockJUnit4ClassRunner
 import org.junit.runners.model.FrameworkMethod
 
-class USpekJUnitRunner(private val testClass: Class<Any>) : BlockJUnit4ClassRunner(testClass) {
+class USpekJUnitRunner(testClass: Class<Any>) : BlockJUnit4ClassRunner(testClass) {
+
+    private val suite = Description.createSuiteDescription(testClass.simpleName)
 
     override fun runChild(method: FrameworkMethod, notifier: RunNotifier) {
         USpek.log = { report ->
             println("Report: $report")
             when (report) {
-                is USpek.Report.Start -> notifier.fireTestStarted(newTestDescription(report))
-                is USpek.Report.Success -> notifier.fireTestFinished(newTestDescription(report))
-                is USpek.Report.Failure -> notifier.fireTestFailure(Failure(newTestDescription(report), report.cause))
+                is USpek.Report.Start -> {
+                    val testDescription = newTestDescription(method, report.testName)
+                    suite.children.first().addChild(testDescription)
+                    notifier.fireTestStarted(testDescription)
+                    notifier.fireTestFinished(testDescription)
+                }
+//                is USpek.Report.Success -> notifier.fireTestFinished(newTestDescription(method, report))
+//                is USpek.Report.Failure -> notifier.fireTestFailure(Failure(newTestDescription(method, report), report.cause))
             }
         }
         println("USpek is running ${method.name}")
+        suite.addChild(Description.createSuiteDescription(method.name))
         methodBlock(method).evaluate()
         super.runChild(method, notifier)
     }
@@ -27,6 +34,6 @@ class USpekJUnitRunner(private val testClass: Class<Any>) : BlockJUnit4ClassRunn
         super.run(notifier)
     }
 
-    private fun newTestDescription(report: USpek.Report) =
-            Description.createTestDescription(testClass.simpleName, "")
+    private fun newTestDescription(method: FrameworkMethod, name: String) =
+            Description.createTestDescription(method.name, name)
 }
