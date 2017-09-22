041ab24168afd3e49b337c3e1fb1ec9f0e341548
Paweł Gajda <pawegio@gmail.com>
2017-09-22 13:38:00 +0200

    Clean-up runner

--- uspek/src/main/java/mareklangiewicz/pl/uspek/USpekJUnitRunner.kt
+++ uspek/src/main/java/mareklangiewicz/pl/uspek/USpekJUnitRunner.kt
@@ -7,67 +7,15 @@ import java.util.*
 
 class USpekJUnitRunner(testClass: Class<Any>) : Runner() {
 
-    enum class TestState { STARTED, SUCCESS, FAILURE }
-
-    data class TestTree(
-            var name: String = "",
-            var location: USpek.CodeLocation? = null,
-            var state: TestState = TestState.STARTED,
-            var assertionLocation: USpek.CodeLocation? = null,
-            var failureCause: Throwable? = null,
-            val subtests: MutableList<TestTree> = mutableListOf()
-    )
-
-    private var testTree: TestTree? = null
-
-    private var currentTest: TestTree? = null
-
     private val rootDescription = Description.createSuiteDescription(testClass.simpleName, UUID.randomUUID().toString())
 
+    private val treeCollectionLogger = TreeCollectionLogger()
+
     init {
-        USpek.log = { report ->
-            when (report) {
-                is USpek.Report.Start -> {
-                    if (testTree === null) {
-                        check(currentTest === null)
-                        testTree = TestTree(report.testName, report.testLocation)
-                        currentTest = testTree
-                    } else {
-                        val test = currentTest!!
-                        val subtest = test.subtests.find { it.location == report.testLocation }
-                        if (subtest !== null) {
-                            currentTest = subtest
-                        } else {
-                            val newtest = TestTree(report.testName, report.testLocation)
-                            test.subtests.add(newtest)
-                            currentTest = newtest
-                        }
-                    }
-                }
-
-                is USpek.Report.Success -> {
-                    val test = currentTest!!
-                    check(testTree !== null)
-                    check(report.testLocation == test.location)
-                    test.state = TestState.SUCCESS
-                    currentTest = testTree // now, we will start again from the top
-                }
-
-                is USpek.Report.Failure -> {
-                    val test = currentTest!!
-                    check(testTree !== null)
-                    check(report.testLocation == test.location)
-                    test.state = TestState.FAILURE
-                    test.assertionLocation = report.assertionLocation
-                    test.failureCause = report.cause
-                    currentTest = testTree // now, we will start again from the top
-                }
-            }
-        }
+        USpek.log = treeCollectionLogger
         val instance = testClass.newInstance()
         testClass.declaredMethods.forEach { it.invoke(instance) }
-        testTree?.state = TestState.SUCCESS
-        rootDescription.addChild(createDescriptions(testTree!!, testClass.name))
+        rootDescription.addChild(createDescriptions(treeCollectionLogger.testTree!!, testClass.name))
     }
 
     override fun getDescription(): Description {
