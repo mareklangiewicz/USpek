534a0d8395819e5a3b927b4c1663245f82313c4e
Paweł Gajda <pawegio@gmail.com>
2017-09-22 10:31:23 +0200

    Revert tests remove

---
+++ uspek/src/test/java/mareklangiewicz/pl/uspek/USpekRunnerTest.kt
@@ -0,0 +1,25 @@
+package mareklangiewicz.pl.uspek
+
+import mareklangiewicz.pl.uspek.USpek.o
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(USpekJUnitRunner::class)
+class USpekRunnerTest {
+
+    @Test
+    fun some_junit_test() {
+        USpek.uspek("some nested test") {
+            "1st assertion" o {
+                Assert.assertTrue(true)
+                "nested assertion" o {
+                    Assert.assertTrue(true)
+                }
+            }
+            "2nd assertion" o {
+                Assert.assertFalse(false)
+            }
+        }
+    }
+}
