0d529f561ed5407fd87fa65c8ed79cb0819d191c
ghostbuster91 <kghost0@gmail.com>
2017-09-23 11:48:54 +0200

    TreeCollectorLogger: test single failure node

--- uspek/src/test/java/mareklangiewicz/pl/uspek/TreeCollectorLoggerTest.kt
+++ uspek/src/test/java/mareklangiewicz/pl/uspek/TreeCollectorLoggerTest.kt
@@ -3,17 +3,32 @@ package mareklangiewicz.pl.uspek
 import mareklangiewicz.pl.uspek.loggers.TestState
 import mareklangiewicz.pl.uspek.loggers.TestTree
 import mareklangiewicz.pl.uspek.loggers.TreeCollectorLogger
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class TreeCollectorLoggerTest {
+    private val logger = TreeCollectorLogger()
 
     @Test
     fun `should create single success node test tree`() {
-        val logger = TreeCollectorLogger()
         val location = CodeLocation("test.kt", 1)
         logger(Report.Start("first test", location))
         logger(Report.Success(location))
-        Assert.assertEquals(logger.testTree!!, TestTree(name = "first test", state = TestState.SUCCESS, location = location))
+        assertEquals(logger.testTree!!, TestTree(name = "first test", state = TestState.SUCCESS, location = location))
+    }
+
+    @Test
+    fun `should create single failure node test tree`() {
+        val location = CodeLocation("test.kt", 1)
+        val assertionLocation = CodeLocation("test.kt", 2)
+        val failureCause = RuntimeException()
+        logger(Report.Start("first test", location))
+        logger(Report.Failure(location, assertionLocation = assertionLocation, cause = failureCause))
+        assertEquals(logger.testTree!!,
+                TestTree(name = "first test",
+                        state = TestState.FAILURE,
+                        location = location,
+                        assertionLocation = assertionLocation,
+                        failureCause = failureCause))
     }
 }
