38cc745cdbeb9e932e89591ac8d345589aa59f0b
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-09-24 11:02:23 +0200

    Rewrite kotlinsample build file to kotlin

---
+++ kotlinsample/build.gradle.kts
@@ -0,0 +1,24 @@
+import org.gradle.kotlin.dsl.kotlin
+import java.net.URI
+
+plugins {
+    kotlin("jvm", "1.1.50")
+    application
+}
+
+application {
+    mainClassName = "pl.mareklangiewicz.uspek.MainKt"
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = URI("https://jitpack.io") }
+}
+
+dependencies {
+    implementation(kotlin("stdlib", "1.1.50"))
+    implementation("junit:junit:4.12")
+    testImplementation("com.github.langara:USpek:9c850d3e36")
+}
+
---
+++ kotlinsample/src/test/java/pl/mareklangiewicz/uspek/DummyTest.kt
@@ -0,0 +1,11 @@
+package pl.mareklangiewicz.uspek
+
+import org.junit.Test
+
+class DummyTest {
+
+    @Test
+    fun bla() {
+        println("bla")
+    }
+}
