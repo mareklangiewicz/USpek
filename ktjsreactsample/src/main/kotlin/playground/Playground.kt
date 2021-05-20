dcdd3a038792418ca29816557ece34bc1d37d36b
Marek Langiewicz <marek.langiewicz@gmail.com>
2021-05-20 15:06:04 +0200

    update

--- ktjsreactsample/src/main/kotlin/playground/Playground.kt
+++ ktjsreactsample/src/main/kotlin/playground/Playground.kt
@@ -4,58 +4,51 @@ import react.*
 import react.dom.*
 import kotlinx.coroutines.*
 import kotlinx.html.js.onClickFunction
-import kotlinx.html.js.onMouseMoveFunction
-import kotlinx.html.onMouseMove
 import painting.clearCanvas
 import painting.paintSomething
 import painting.pickColor
-import pl.mareklangiewicz.uspek.USpekTree
 import pl.mareklangiewicz.uspek.finished
 import pl.mareklangiewicz.uspek.status
 import pl.mareklangiewicz.uspek.uspekContext
 import pl.mareklangiewicz.uspek.uspekLog
 
 external interface PlaygroundProps : RProps { var speed: Int }
-external interface PlaygroundState : RState { var tree: USpekTree }
 
-@JsExport
-class Playground(props: PlaygroundProps) : RComponent<PlaygroundProps, PlaygroundState>(props) {
+val Playground = functionalComponent<PlaygroundProps> { props ->
 
-    override fun PlaygroundState.init(props: PlaygroundProps) {
-        tree = uspekContext.root
+    var tree by useState(uspekContext.root)
+
+    useEffect(emptyList()) {
         uspekLog = {
             println(it.status)
-            setState { tree = uspekContext.root }
+            // we don't need any setState here because tree is useState hook delegate property
+            tree = uspekContext.root.copy() // FIXME: is copy needed? check (and debug!) how react compares states
             paintSomething()
-//            delay(40) // FIXME: make it work (it works on my homepage (with suspending uspek copied&pasted))
-                // probably all uspek functions have to be either suspending or inline...
-                // maybe it's possible to force react to update DOM, then sleep for 40ms (so it's animating and browser
-                // have a chance to refresh each time) - probably not (without loosing stack frames / breaking uspek control flow)..
+    //            delay(40) // FIXME: make it work (it works on my homepage (with suspending uspek copied&pasted))
+            // probably all uspek functions have to be either suspending or inline...
+            // maybe it's possible to force react to update DOM, then sleep for 40ms (so it's animating and browser
+            // have a chance to refresh each time) - probably not (without loosing stack frames / breaking uspek control flow)..
 
             if (it.finished) clearCanvas()
         }
-    }
-
-    override fun componentDidMount() {
-        GlobalScope.launch {
+        // FIXME: We should have scope cancelling when leaving playground
+        MainScope().launch {
+            delay(500) // temporary delay for experiments
             example()
             paintSomething()
         }
     }
-
-    override fun RBuilder.render() {
-        div(classes = "playground") {
-            div(classes = "tests-side") { rtree(state.tree) }
-            div(classes = "canvas-side") {
-                div(classes = "canvas") {
-                    canvas {
-                        attrs {
-                            onClickFunction = {
-                                val event = it.asDynamic()
-                                val color = pickColor(event.clientX - event.target.offsetLeft, event.clientY - event.target.offsetTop)
-                                // TODO: check what clientX/Y is
-                                println(color)
-                            }
+    div(classes = "playground") {
+        div(classes = "tests-side") { rtree(tree) }
+        div(classes = "canvas-side") {
+            div(classes = "canvas") {
+                canvas {
+                    attrs {
+                        onClickFunction = {
+                            val event = it.asDynamic()
+                            val color = pickColor(event.clientX - event.target.offsetLeft, event.clientY - event.target.offsetTop)
+                            // TODO: check what clientX/Y is
+                            println(color)
                         }
                     }
                 }
@@ -64,4 +57,4 @@ class Playground(props: PlaygroundProps) : RComponent<PlaygroundProps, Playgroun
     }
 }
 
-fun RBuilder.playground(speed: Int = 400) = child(Playground::class) { attrs.speed = speed }
+fun RBuilder.playground(speed: Int = 400) = child(Playground) { attrs.speed = speed }
